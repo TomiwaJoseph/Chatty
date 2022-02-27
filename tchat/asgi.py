edf84d12a0542cbbbbf1779c26d084d7c765b9c1
TomiwaJoseph <tomiwajoseph88@gmail.com>
2022-02-27 20:33:42 +0100

    added asgi configurations

--- tchat/asgi.py
+++ tchat/asgi.py
@@ -1,21 +1,18 @@
-"""
-ASGI config for tchat project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
-"""
-
 import os
-
+import chat.routing
 from django.core.asgi import get_asgi_application
-from channels.routing import ProtocolTypeRouter
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tchat.settings')
 
 # application = get_asgi_application()
 
-application = ({
-    'http': get_asgi_application()    
+application = ProtocolTypeRouter({
+    'http': get_asgi_application()    ,
+    'websocket': AuthMiddlewareStack(
+        URLRouter(
+            chat.routing.websocket_urlpatterns
+        )
+    )
 })
