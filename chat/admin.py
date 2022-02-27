dc5a37e76d9eecbaf0221a64d4686aa4e7a5e88a
TomiwaJoseph <tomiwajoseph88@gmail.com>
2022-02-27 20:31:08 +0100

    added form validation to prevent multiple thread by same users

--- chat/admin.py
+++ chat/admin.py
@@ -1,3 +1,34 @@
 from django.contrib import admin
+from django import forms
+from django.core.exceptions import ValidationError
+from django.db.models import Q
+from .models import Thread, ChatMessage
 
-# Register your models here.
+admin.site.register(ChatMessage)
+
+class ChatMessage(admin.TabularInline):
+    model = ChatMessage
+    
+
+class ThreadForm(forms.ModelForm):
+    def clean(self):
+        """
+        This is the function that can be used to
+        validate your model data from admin
+        """
+        super(ThreadForm, self).clean()
+        first_person = self.cleaned_data.get('first_person')
+        second_person = self.cleaned_data.get('second_person')
+        if not self.instance.id:
+            raise ValidationError(f'Thread between {first_person} and {second_person} already exists.')
+
+
+class ThreadAdmin(admin.ModelAdmin):
+    inlines = [ChatMessage]
+    form = ThreadForm
+    class Meta:
+        model = Thread
+
+
+admin.site.register(Thread, ThreadAdmin)
+
