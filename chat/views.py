4ccd865d1b2306330bf2cfc075d984050bf1f32d
TomiwaJoseph <tomiwajoseph88@gmail.com>
2022-02-27 20:27:00 +0100

    removed ajax request view

--- chat/views.py
+++ chat/views.py
@@ -2,16 +2,17 @@ from django.shortcuts import render
 from django.http import JsonResponse
 from django.template.loader import render_to_string
 from django.contrib.auth.decorators import login_required
+from users.models import Profile
+from .models import Thread
 
-
-# -------------- Ajax requests --------------------
-
-def show_chat_messages(request):
-    t = render_to_string('chat/ajax_pages/chat_messages.html')
-    return JsonResponse({"conversation": t})
-
-# -------------- Ajax requests --------------------
 
 @login_required
 def chathouse(request):
-    return render(request, 'chat/welcome_view.html')
+    profile_object = Profile.objects.get(user=request.user).profile_picture
+    logged_user_picture = profile_object.url
+    threads = Thread.objects.by_user(user=request.user).prefetch_related('chatmessage_thread').order_by('timestamp')
+    context = {
+        "logged_user_picture": logged_user_picture,
+        "Threads": threads,
+    }
+    return render(request, 'chat/welcome_view.html', context)
