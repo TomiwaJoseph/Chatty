3cf30f12b0e05999545c5b7782abf9172cbd4f28
TomiwaJoseph <tomiwajoseph88@gmail.com>
2022-03-13 00:23:04 +0100

    removed unused js variables and events

--- chat/static/chat/js/main.js
+++ chat/static/chat/js/main.js
@@ -4,43 +4,52 @@ $(document).ready(function () {
         $('input').blur();
     })
 
+    $('.message_tins #report_contact').on('click', function() {
+        $('#reportUserModal').modal("show")
+    })
+
+    $('#statusModal .modal-body span').on('click', function() {
+        innerText = $(this).html()
+        if (innerText == "Refresh Page"){
+            location.reload();
+        }
+    })
+
+    $('.message_tins #search_chat_text').on('click', function() {
+        // $('#searchChatModal').modal("show");
+        $('#statusModal .modal-body h4').html('Info!')
+        $('#statusModal .icon-box i').html('&#33;')
+        $('#statusModal .modal-body span').html('Continue Chatting')
+        $('#statusModal .modal-header').css('background', '#17a2b8')
+        $('#statusModal .modal-body p').html('Simply press Ctrl + F to use the powerful in-built browser finder to find your chat message. This fine utility is not available at this time.')
+        $('#statusModal').modal('show');
+    })
+
+    $('#add_cont').on('click', function() {
+        $('#addContactModal').modal("show")
+    })
+
+    $('#nxt_action').on('click', function() {
+        error_status = $(this).html()
+        if (error_status == "Try Again") {
+            $('#editPasswordModal').modal('show');
+        }
+    })
+
     $('#user_action_menu_btn').on('click', function () {
         $('.user_action_menu').fadeToggle();
     })
 
-    $('#action_menu_btn').on('click', function () {
+    $('.message_tins #action_menu_btn').on('click', function () {
         $('.action_menu').fadeToggle();
     })
-    
-    $('.message_tins .fa-search').on('click', function(e) {
-        e.stopPropagation();
-        $('.chat_search_form').fadeToggle();
-        document.getElementById("search_text").focus();
-    });
-
-    $('.contacts_card .fa-plus').on('click', function(e) {
-        $('.contact_add_form').fadeToggle();
-        document.getElementById("add_new_cont").focus();
-        if ($('.contact_search').is(':visible')) {
-            $('.contact_search').fadeOut();
-        } else {
-            $('.contact_search').fadeIn();
-        }
-    });
 
     $(document).click(function (e) {
-        if(!$(e.target).closest('.chat_search_form').length){
-            $('.chat_search_form').fadeOut();
-        }
         if(!$(e.target).closest('#user_action_menu_btn').length){
             $('.user_action_menu').fadeOut();
         }
         if(!$(e.target).closest('#action_menu_btn').length){
             $('.action_menu').fadeOut();
-        }
-        if(!$(e.target).closest('#add_cont').length){
-            $('.contact_add_form').fadeOut();
-            $('.contact_search').fadeIn();
         }
     });
 
