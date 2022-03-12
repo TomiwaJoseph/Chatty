0672945233d1d668f722c3644abb7cb9eca4ff37
TomiwaJoseph <tomiwajoseph88@gmail.com>
2022-03-13 00:37:57 +0100

    added report contact to admin

--- chat/admin.py
+++ chat/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django import forms
 from django.core.exceptions import ValidationError
 from django.db.models import Q
-from .models import Thread, ChatMessage
+from .models import Thread, ChatMessage, ReportContact
 
 admin.site.register(ChatMessage)
 
@@ -26,9 +26,16 @@ class ThreadForm(forms.ModelForm):
 class ThreadAdmin(admin.ModelAdmin):
     inlines = [ChatMessage]
     form = ThreadForm
+    list_display = ['first_person', 'second_person', 'blocked']
+    list_editable = ['blocked',]
     class Meta:
         model = Thread
+        
+        
+class ReportContactAdmin(admin.ModelAdmin):
+    list_display = ['complainant', 'offender', 'subject', 'reason']
 
 
+admin.site.register(ReportContact, ReportContactAdmin)
 admin.site.register(Thread, ThreadAdmin)
 
